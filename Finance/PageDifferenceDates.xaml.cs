d047033a5a0b62712085cfabd3ca60eab4ad8a21
Geert Geerits <ggeerits@hotmail.com>
2022-11-19 10:12:53 +0100

    Add project files.

---
+++ Finance/PageDifferenceDates.xaml.cs
@@ -0,0 +1,245 @@
+﻿using Finance.Resources.Languages;
+
+namespace Finance;
+public partial class PageDifferenceDates : ContentPage
+{
+    public PageDifferenceDates()
+    {
+        try
+        {
+            InitializeComponent();
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("InitializeComponent", ex.Message, MainPage.cButtonCloseText);
+            return;
+        }
+
+        // Put text in the chosen language in the controls.
+        lblTitle.Text = FinLang.DifferenceDatesForm_Text;
+
+        lblExplanation.Text = FinLang.DifferenceExplanationDates_Text;
+        lblDate1.Text = FinLang.Date1_Text;
+        lblDate2.Text = FinLang.Date2_Text;
+        lblDateDifferenceDays.Text = FinLang.DateDifferenceDays_Text;
+        lblDateDifferenceHours.Text = FinLang.DateDifferenceHours_Text;
+        lblDateDifferenceMinutes.Text = FinLang.DateDifferenceMinutes_Text;
+        lblDateDifferenceSeconds.Text = FinLang.DateDifferenceSeconds_Text;
+        lblDateDifferenceYearMonthDay.Text = FinLang.DateDifferenceYearMonthDay_Text;
+
+        btnCalculate.Text = FinLang.Calculate_Text;
+        btnReset.Text = FinLang.Reset_Text;
+
+        // Set the date properties for the DatePicker.
+        dtpDate1.MinimumDate = new DateTime(1583, 1, 1);
+        dtpDate1.MaximumDate = new DateTime(3999, 12, 31);
+        dtpDate2.MinimumDate = new DateTime(1583, 1, 1);
+        dtpDate2.MaximumDate = new DateTime(3999, 12, 31);
+
+        dtpDate1.Format = MainPage.cDateFormat;
+        dtpDate2.Format = MainPage.cDateFormat;
+
+        // Set focus to the first entry field.
+        dtpDate1.Focus();
+    }
+
+    // Clear result fields if the date have changed.
+    private void DatePickerDateSelected(object sender, EventArgs e)
+    {
+        txtDateDifferenceDays.Text = "";
+        txtDateDifferenceHours.Text = "";
+        txtDateDifferenceMinutes.Text = "";
+        txtDateDifferenceSeconds.Text = "";
+        txtDateDifferenceYearMonthDay.Text = "";
+    }
+
+    // Go to the next field when the return key have been pressed.
+    private void GoToNextField(object sender, EventArgs e)
+    {
+        if (sender == dtpDate1)
+        {
+            dtpDate2.Focus();
+        }
+    }
+
+    // Calculate the result.
+    private void CalculateResult(object sender, EventArgs e)
+    {
+        // Calculate the date difference in days, hours, minutes and seconds.
+        decimal nDateDifference = (dtpDate2.Date - dtpDate1.Date).Days;
+        txtDateDifferenceDays.Text = MainPage.RoundDecimalToNumDecimals(ref nDateDifference, 0, "N");
+        
+        nDateDifference *= 24;
+        txtDateDifferenceHours.Text = MainPage.RoundDecimalToNumDecimals(ref nDateDifference, 0, "N");
+
+        nDateDifference *= 60;
+        txtDateDifferenceMinutes.Text = MainPage.RoundDecimalToNumDecimals(ref nDateDifference, 0, "N");
+
+        nDateDifference *= 60;
+        txtDateDifferenceSeconds.Text = MainPage.RoundDecimalToNumDecimals(ref nDateDifference, 0, "N");
+
+        // Calculate the date difference in years, months and days.
+        DateDifference dateDifference = new(dtpDate1.Date, dtpDate2.Date);
+        txtDateDifferenceYearMonthDay.Text = dateDifference.ToString();
+
+        // Set focus.
+        btnCalculate.Focus();
+    }
+
+    // Reset the entry and result fields.
+    private void ResetEntryFields(object sender, EventArgs e)
+    {
+        dtpDate1.Date = DateTime.Today;
+        dtpDate2.Date = DateTime.Today;
+        txtDateDifferenceDays.Text = "";
+        txtDateDifferenceHours.Text = "";
+        txtDateDifferenceMinutes.Text = "";
+        txtDateDifferenceSeconds.Text = "";
+        txtDateDifferenceYearMonthDay.Text = "";
+    }
+}
+
+// Class to calculate the date difference in years, months and days.
+// Source: https://www.codeproject.com/Articles/28837/Calculating-Duration-Between-Two-Dates-in-Years-Mo
+// by: Mohammed Ali Babu
+public class DateDifference
+{
+    // Defining Number of days in month; index 0=> january and 11=> december.
+    // February contain either 28 or 29 days, that's why here value is -1 which wil be calculate later.
+    private readonly int[] monthDay = new int[12] { 31, -1, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    // Contain from date.
+    private readonly DateTime fromDate;
+
+    // Contain to date.
+    private readonly DateTime toDate;
+
+    // This three variable for output representation.
+    private readonly int year;
+    private readonly int month;
+    private readonly int day;
+
+    public DateDifference(DateTime d1, DateTime d2)
+    {
+        int increment;
+
+        if (d1 > d2)
+        {
+            fromDate = d2;
+            toDate = d1;
+        }
+        else
+        {
+            fromDate = d1;
+            toDate = d2;
+        }
+
+        // Day Calculation.
+        increment = 0;
+
+        if (fromDate.Day > toDate.Day)
+        {
+            increment = monthDay[fromDate.Month - 1];
+        }
+
+        // If it is february month.
+        // If it's to day is less then from day.
+        if (increment == -1)
+        {
+            if (DateTime.IsLeapYear(fromDate.Year))
+            {
+                // Leap year february contain 29 days.
+                increment = 29;
+            }
+            else
+            {
+                increment = 28;
+            }
+        }
+        
+        if (increment != 0)
+        {
+            day = (toDate.Day + increment) - fromDate.Day;
+            increment = 1;
+        }
+        else
+        {
+            day = toDate.Day - fromDate.Day;
+        }
+
+        // Month calculation.
+        if ((fromDate.Month + increment) > toDate.Month)
+        {
+            month = (toDate.Month + 12) - (fromDate.Month + increment);
+            increment = 1;
+        }
+        else
+        {
+            month = (toDate.Month) - (fromDate.Month + increment);
+            increment = 0;
+        }
+
+        // Year calculation.
+        year = toDate.Year - (fromDate.Year + increment);
+    }
+
+    public override string ToString()
+    {
+        string cYear;
+        string cMonth;
+        string cDay;
+
+        if (year == 1)
+        {
+            cYear = " " + FinLang.DateYear_Text + ", ";
+        }
+        else
+        {
+            cYear = " " + FinLang.DateYears_Text + ", ";
+        }
+
+        if (month == 1)
+        {
+            cMonth = " " + FinLang.DateMonth_Text + ", ";
+        }
+        else
+        {
+            cMonth = " " + FinLang.DateMonths_Text + ", ";
+        }
+
+        if (day == 1)
+        {
+            cDay = " " + FinLang.DateDay_Text;
+        }
+        else
+        {
+            cDay = " " + FinLang.DateDays_Text;
+        }
+
+        return year.ToString() + cYear + month.ToString() + cMonth + day.ToString() + cDay;
+    }
+
+    public int Years
+    {
+        get
+        {
+            return year;
+        }
+    }
+
+    public int Months
+    {
+        get
+        {
+            return month;
+        }
+    }
+
+    public int Days
+    {
+        get
+        {
+            return day;
+        }
+    }
+}
---
+++ Finance/PageSettings.xaml.cs
@@ -0,0 +1,241 @@
+using Finance.Resources.Languages;
+using System.Diagnostics;
+
+namespace Finance;
+
+public partial class PageSettings : ContentPage
+{
+    // Local variables.
+    private readonly Stopwatch stopWatch = new();
+
+    public PageSettings()
+    {        
+        try
+        {
+            InitializeComponent();
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("InitializeComponent PageSettings", ex.Message, MainPage.cButtonCloseText);
+            return;
+        }
+
+        // Put text in the chosen language in the controls.
+        lblTitle.Text = FinLang.Settings_Text;
+
+        lblTheme.Text = FinLang.Theme_Text;
+        rbnThemeSystem.Content = FinLang.System_Text;
+        rbnThemeLight.Content = FinLang.Light_Text;
+        rbnThemeDark.Content = FinLang.Dark_Text;
+        lblDateFormat.Text = FinLang.DateFormat_Text;
+        rbnDateFormatSystem.Content = FinLang.System_Text;
+        rbnDateFormatISO8601.Content = FinLang.DateISO8601_Text;
+        lblPageFormat.Text = FinLang.PageFormat_Text;
+        rbnPageFormatA4.Content = FinLang.PageA4_Text;
+        rbnPageFormatLetter.Content = FinLang.PageLetter_Text;
+        lblRoundNumber.Text = FinLang.RoundNumber_Text;
+        rbnRoundNumberAwayFromZero.Content = FinLang.RoundNumberAwayFromZero_Text;
+        rbnRoundNumberToEven.Content = FinLang.RoundNumberToEven_Text;
+        lblKeyboard.Text = FinLang.Keyboard_Text;
+        rbnKeyboardDefault.Content = FinLang.Default_Text;
+        rbnKeyboardNumeric.Content = FinLang.Numeric_Text;
+        rbnKeyboardText.Content = FinLang.Text_Text;
+        btnSettingsSave.Text = FinLang.Save_Text;
+        btnSettingsReset.Text = FinLang.Reset_Text;
+
+        // Set radiobutton to the used theme.
+        string cCurrentTheme;
+        
+        if (MainPage.cTheme == "")
+        {
+            AppTheme currentTheme = Application.Current.RequestedTheme;
+            cCurrentTheme = Convert.ToString(currentTheme);
+        }
+        else
+        {
+            cCurrentTheme = MainPage.cTheme;
+        }
+        //DisplayAlert("cCurrentTheme", cCurrentTheme, "OK");  // For testing
+
+        if (cCurrentTheme == "Light")
+        {
+            rbnThemeLight.IsChecked = true;
+        }
+        else if (cCurrentTheme == "Dark")
+        {
+            rbnThemeDark.IsChecked = true;
+        }
+        else
+        {
+            rbnThemeSystem.IsChecked = true;
+        }
+
+        // Set radiobutton to the date format.
+        if (MainPage.bDateFormatSystem == true)
+        {
+            rbnDateFormatSystem.IsChecked = true;
+        }
+        else
+        {
+            rbnDateFormatISO8601.IsChecked = true;
+        }
+
+        // Set radiobutton to the page format.
+        if (MainPage.cPageFormat == "A4")
+        {
+            rbnPageFormatA4.IsChecked = true;
+        }
+        else
+        {
+            rbnPageFormatLetter.IsChecked = true;
+        }
+
+        // Set radiobutton to the rounding numbers method.
+        if (MainPage.cRoundNumber == "AwayFromZero")
+        {
+            rbnRoundNumberAwayFromZero.IsChecked = true;
+        }
+        else
+        {
+            rbnRoundNumberToEven.IsChecked = true;
+        }
+
+        // Set radiobutton to the used keyboard.
+        if (MainPage.cKeyboard == "Default")
+        {
+            rbnKeyboardDefault.IsChecked = true;
+        }
+        else if (MainPage.cKeyboard == "Numeric")
+        {
+            rbnKeyboardNumeric.IsChecked = true;
+        }
+        else
+        {
+            rbnKeyboardText.IsChecked = true;
+        }
+
+        // Start the stopWatch for resetting all the settings.
+        stopWatch.Start();
+    }
+
+    // Radio button themes clicked event.
+    private void OnThemesRadioButtonCheckedChanged(object sender, EventArgs e)
+    {
+        if (rbnThemeSystem.IsChecked)
+        {
+            MainPage.cTheme = "System";
+        }
+        else if (rbnThemeLight.IsChecked)
+        {
+            MainPage.cTheme = "Light";
+        }
+        else if (rbnThemeDark.IsChecked)
+        {
+            MainPage.cTheme = "Dark";
+        }
+    }
+
+    // Radio button date format clicked event.
+    private void OnDateFormatRadioButtonCheckedChanged(object sender, EventArgs e)
+    {
+        if (rbnDateFormatSystem.IsChecked)
+        {
+            MainPage.bDateFormatSystem = true;
+            MainPage.cDateFormat = MainPage.cSysDateFormat;
+        }
+        else if (rbnDateFormatISO8601.IsChecked)
+        {
+            MainPage.bDateFormatSystem = false;
+            MainPage.cDateFormat = "yyyy-MM-dd";
+        }
+    }
+
+    // Radio button page format clicked event.
+    private void OnPageFormatRadioButtonCheckedChanged(object sender, EventArgs e)
+    {
+        if (rbnPageFormatA4.IsChecked)
+        {
+            MainPage.cPageFormat = "A4";
+        }
+        else if (rbnPageFormatLetter.IsChecked)
+        {
+            MainPage.cPageFormat = "Letter";
+        }
+    }
+
+    // Radio button roundig numbers method clicked event.
+    private void OnRoundNumberRadioButtonCheckedChanged(object sender, EventArgs e)
+    {
+        if (rbnRoundNumberAwayFromZero.IsChecked)
+        {
+            MainPage.cRoundNumber = "AwayFromZero";
+        }
+        else if (rbnRoundNumberToEven.IsChecked)
+        {
+            MainPage.cRoundNumber = "ToEven";
+        }
+    }
+
+    // Radio button keyboard clicked event.
+    private void OnKeyboardRadioButtonCheckedChanged(object sender, EventArgs e)
+    {
+        if (rbnKeyboardDefault.IsChecked)
+        {
+            MainPage.cKeyboard = "Default";
+        }
+        else if (rbnKeyboardNumeric.IsChecked)
+        {
+            MainPage.cKeyboard = "Numeric";
+        }
+        else if (rbnKeyboardText.IsChecked)
+        {
+            MainPage.cKeyboard = "Text";
+        }
+    }
+
+    // Button save settings clicked event.
+    private void OnSettingsSaveClicked(object sender, EventArgs e)
+    {
+        Preferences.Default.Set("SettingTheme", MainPage.cTheme);
+        Preferences.Default.Set("SettingDateFormatSystem", MainPage.bDateFormatSystem);
+        Preferences.Default.Set("SettingPageFormat", MainPage.cPageFormat);
+        Preferences.Default.Set("SettingRoundNumber", MainPage.cRoundNumber);
+        Preferences.Default.Set("SettingKeyboard", MainPage.cKeyboard);
+        Preferences.Default.Set("SettingLanguage", MainPage.cLanguage);
+
+        // Wait 500 milliseconds otherwise the settings are not saved in Android.
+        Task.Delay(500).Wait();
+
+        // Restart the application.
+        Application.Current.MainPage = new AppShell();
+    }
+
+    // Button reset settings clicked event.
+    private void OnSettingsResetClicked(object sender, EventArgs e)
+    {
+        // Get the elapsed time in milli seconds.
+        stopWatch.Stop();
+
+        if (stopWatch.ElapsedMilliseconds < 2001)
+        {
+            // Clear all settings after the first clicked event within the first 2 seconds after opening the setting page.
+            Preferences.Default.Clear();
+        }
+        else
+        {
+            // Reset some settings.
+            Preferences.Default.Remove("SettingTheme");
+            Preferences.Default.Remove("SettingDateFormatSystem");
+            Preferences.Default.Remove("SettingPageFormat");
+            Preferences.Default.Remove("SettingRoundNumber");
+            Preferences.Default.Remove("SettingKeyboard");
+            Preferences.Default.Remove("SettingLanguage");
+        }
+
+        // Wait 500 milliseconds otherwise the settings are not saved in Android.
+        Task.Delay(500).Wait();
+
+        // Restart the application.
+        Application.Current.MainPage = new AppShell();
+    }
+}
