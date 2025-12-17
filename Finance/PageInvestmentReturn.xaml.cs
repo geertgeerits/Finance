b0ee1cab118f4ac15fa343996eb2eeaa5a2d3ede
Geert Geerits <ggeerits@hotmail.com>
2025-12-17 08:38:06 +0100

    Update NuGet package

--- Finance/PageInvestmentReturn.xaml.cs
+++ Finance/PageInvestmentReturn.xaml.cs
@@ -229,7 +229,7 @@ namespace Finance
             catch (Exception ex)
             {
 #if DEBUG                
-                DisplayAlertAsync(FinLang.ErrorTitle_Text, ex.Message, FinLang.ButtonClose_Text);
+                await DisplayAlertAsync(FinLang.ErrorTitle_Text, ex.Message, FinLang.ButtonClose_Text);
 #endif
                 ResetEntryFields(null, null);
                 return;
